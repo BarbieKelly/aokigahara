061af40d8412ac50311f5c207d053a7de7065782
Barbie <barbara.kelly892@gmail.com>
2020-03-05 21:57:19 -0600

    changes to directory and menu item components. initiated shop page and shop data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import { Router, Route, Switch } from "react-router";
+import { Router, Route, Link, Switch } from "react-router-dom";
 
 import React from 'react';
 import './App.css';
 
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Route exact path='/' component={Homepage} />
+      <Route exact path='/shop' component={ShopPage} />
     </div>
   );
 }
