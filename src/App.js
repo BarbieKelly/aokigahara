792f5afc65f113a8d5d910dfad510843a9b45638
Chelsea Basinger <chelsea.basinger@gmail.com>
2020-03-07 12:58:30 -0500

    Added Sign-Up form that adds users who manually sign up with email and password to our database

--- src/App.js
+++ src/App.js
@@ -33,6 +33,8 @@ class App extends React.Component {
               ...snapShot.data()
             }
           });
+
+          console.log(this.state);
         });
       }
       //If userAuth = null 
